6456d8e921b58b80fc86502599df2e84dfd2927e
Vinicius Araujo Ramos <124802745+viniciusaraujoramos@users.noreply.github.com>
2024-04-11 09:02:31 -0300

    Add files via upload

---
+++ Dados/Dados/Dados/Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dados
+{
+    class Pessoa
+    {
+        private string nome;
+        private DateTime dataNascimento;
+        private float altura;
+    }
+
+}
