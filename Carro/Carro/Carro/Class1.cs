e7b5d53b183e3c9a8427c4a0704f7953fb524114
Vinicius Araujo Ramos <124802745+viniciusaraujoramos@users.noreply.github.com>
2024-04-12 21:16:31 -0300

    Add files via upload

---
+++ Carro/Carro/Carro/Class1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Carro
+{
+    public class Class1
+    {
+        private double Velocidade { get; set; }
+        private double Direcao { get; set; }
+
+        public void AumentaVel(double mais)
+        {
+            if (Velocidade+mais > 200)
+            {
+                Console.WriteLine("Você não pode acelerar tanto, pare no 200.");
+            }
+            else
+            {
+                Velocidade = Velocidade + mais;
+                Console.WriteLine("Sua velociade é " + Velocidade+"Km/h");
+            }
+        }
+        public void DiminuiVel(double menos)
+        {
+            if (Velocidade - menos < 0)
+            {
+                Console.WriteLine("Você não pode desacelerar tanto, pare no 0.");
+            }
+            else
+            {
+                Velocidade = Velocidade - menos;
+                Console.WriteLine("Sua velociade é " + Velocidade+"Km/h");
+            }
+        }
+        public void Direita(double mai)
+        {
+
+            Direcao = mai;
+            Console.WriteLine("Você virou "+Direcao+" graus para a direita");
+        
+        }
+        public void Esquerda(double meno)
+        {
+        
+            Direcao = meno;
+            Console.WriteLine("Você virou "+Direcao+" graus para esquerda");
+        
+        }
+    }
+}
