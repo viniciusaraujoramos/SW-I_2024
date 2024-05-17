fb0029ca4a6ae5dfc850b5e3e24d91d9cec879ba
Vinicius Araujo Ramos <124802745+viniciusaraujoramos@users.noreply.github.com>
2024-05-16 22:27:01 -0300

    Add files via upload

---
+++ Exercicios/exe02/exe02/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace exe02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Exemplo de uso da classe RegistroDeCompras
+            RegistroDeCompras registro = new RegistroDeCompras(DateTime.Now, "Camiseta", 29.99m);
+            registro.AdicionarCompra(DateTime.Now.AddDays(-1), "Calça", 49.99m);
+            registro.AdicionarCompra(DateTime.Now.AddDays(-2), "Tênis", 99.99m);
+
+            // Listar todas as compras
+            registro.ListarCompras();
+        }
+    }
+}
