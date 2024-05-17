93732896b65219aaacc4462af156aafe924e94ed
Vinicius Araujo Ramos <124802745+viniciusaraujoramos@users.noreply.github.com>
2024-05-16 22:37:52 -0300

    Add files via upload

---
+++ Exercicios/exe11/exe11/Carro.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exe11
+{
+    public class Carro
+    {
+        // Declaração de campos privados para armazenar o modelo, o ano e a velocidade
+        private string _modelo;
+        private int _ano;
+        private int _velocidade;
+
+        // Construtor da classe Carro que inicializa os campos do carro com os valores passados
+        public Carro(string modelo, int ano)
+        {
+            _modelo = modelo;
+            _ano = ano;
+            _velocidade = 0; // A velocidade inicial é definida como 0
+        }
+
+        public string Modelo
+        {
+            get { return _modelo; }
+            set { _modelo = value; }
+        }
+
+        public int Ano
+        {
+            get { return _ano; }
+            set { _ano = value; }
+        }
+
+        // Propriedade Velocidade que encapsula o campo _velocidade, permitindo acesso externo apenas para leitura
+        public int Velocidade
+        {
+            get { return _velocidade; }
+        }
+
+        // Método Acelerar que aumenta a velocidade do carro em 10 
+        public void Acelerar()
+        {
+            _velocidade += 10;
+        }
+
+        // Método Frear que diminui a velocidade do carro em 10 , até no mínimo 0
+        public void Frear()
+        {
+            if (_velocidade >= 10)
+            {
+                _velocidade -= 10;
+            }
+            else
+            {
+                _velocidade = 0;
+            }
+        }
+    }
+
+}
---
+++ Exercicios/exe11/exe11/Program.cs
@@ -0,0 +1,46 @@
+﻿namespace exe11
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("My car");
+
+            // Solicita ao usuário informações sobre o carro
+            Console.WriteLine("\nDigite as informações do seu carro:");
+            Console.Write("Modelo: ");
+         
+            string modeloDoCarro = Console.ReadLine();
+            Console.Write("Ano: ");
+            int anoDeFabricacao = int.Parse(Console.ReadLine());
+
+            // Cria um objeto da classe Carro com as informações fornecidas pelo usuário
+            Carro carro = new Carro(modeloDoCarro, anoDeFabricacao);
+
+            // resultado
+            Console.WriteLine("\n Resultado");
+            Console.WriteLine($"Modelo: {carro.Modelo}, Ano: {carro.Ano}, Velocidade: {carro.Velocidade}");
+
+            // Chama o método Acelerar do carro duas vezes e exibe a nova velocidade
+            carro.Acelerar();
+            Console.WriteLine($"Acelerando... Velocidade: {carro.Velocidade}");
+            carro.Acelerar();
+            Console.WriteLine($"Acelerando mais uma vez. Velocidade: {carro.Velocidade}");
+
+            // Chama o método Frear do carro duas vezes e exibe a nova velocidade
+            carro.Frear();
+            Console.WriteLine($"Freando uma vez. Velocidade: {carro.Velocidade}");
+            carro.Frear();
+            Console.WriteLine($"Freando. Velocidade: {carro.Velocidade}");
+
+            // Chama o método Acelerar do carro duas vezes e exibe a nova velocidade
+            carro.Acelerar();
+            Console.WriteLine($"Acelerando mais uma vez. Velocidade: {carro.Velocidade}");
+            carro.Acelerar();
+            Console.WriteLine($"Acelerando mais uma vez. Velocidade: {carro.Velocidade}");
+
+            // Aguarda até que o usuário pressione qualquer tecla para encerrar o programa
+            Console.ReadKey();
+        }
+    }
+}
